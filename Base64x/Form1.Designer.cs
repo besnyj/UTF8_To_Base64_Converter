31a79479923ed1f6b3565fcb4f09223f2dfe8e65
Besnyj <verificacoesfelipe@gmail.com>
2024-04-14 15:06:49 -0300

    improved design

--- Base64x/Form1.Designer.cs
+++ Base64x/Form1.Designer.cs
@@ -38,35 +38,35 @@
             this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // UTF8Entry
+            // UTF8TextBox
             // 
             this.UTF8TextBox.BackColor = System.Drawing.Color.DarkGray;
             this.UTF8TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.UTF8TextBox.ForeColor = System.Drawing.SystemColors.InactiveCaptionText;
             this.UTF8TextBox.Location = new System.Drawing.Point(12, 29);
             this.UTF8TextBox.Name = "UTF8TextBox";
-            this.UTF8TextBox.Size = new System.Drawing.Size(596, 204);
+            this.UTF8TextBox.Size = new System.Drawing.Size(208, 204);
             this.UTF8TextBox.TabIndex = 0;
             this.UTF8TextBox.Text = "";
             this.UTF8TextBox.TextChanged += new System.EventHandler(this.UTF8Entry_TextChanged);
             // 
-            // BASE64Out
+            // BASE64TextBox
             // 
             this.BASE64TextBox.BackColor = System.Drawing.Color.DarkGray;
             this.BASE64TextBox.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.BASE64TextBox.Location = new System.Drawing.Point(12, 266);
             this.BASE64TextBox.Name = "BASE64TextBox";
             this.BASE64TextBox.ReadOnly = true;
-            this.BASE64TextBox.Size = new System.Drawing.Size(596, 204);
+            this.BASE64TextBox.Size = new System.Drawing.Size(208, 204);
             this.BASE64TextBox.TabIndex = 1;
             this.BASE64TextBox.Text = "";
             this.BASE64TextBox.TextChanged += new System.EventHandler(this.UTF8Entry_TextChanged);
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(635, 213);
+            this.button1.Location = new System.Drawing.Point(226, 29);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(63, 47);
+            this.button1.Size = new System.Drawing.Size(63, 204);
             this.button1.TabIndex = 2;
             this.button1.Text = "Swap 🔁";
             this.button1.UseVisualStyleBackColor = true;
@@ -74,9 +74,9 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(635, 266);
+            this.button2.Location = new System.Drawing.Point(226, 266);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(63, 47);
+            this.button2.Size = new System.Drawing.Size(63, 204);
             this.button2.TabIndex = 3;
             this.button2.Text = "Copy";
             this.button2.UseVisualStyleBackColor = true;
@@ -85,8 +85,8 @@
             // label1
             // 
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.SystemColors.InactiveBorder;
-            this.label1.Location = new System.Drawing.Point(279, 9);
+            this.label1.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label1.Location = new System.Drawing.Point(90, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(40, 15);
             this.label1.TabIndex = 4;
@@ -96,8 +96,8 @@
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.SystemColors.Menu;
-            this.label2.Location = new System.Drawing.Point(279, 245);
+            this.label2.ForeColor = System.Drawing.Color.Goldenrod;
+            this.label2.Location = new System.Drawing.Point(90, 245);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(57, 15);
             this.label2.TabIndex = 5;
@@ -106,20 +106,20 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(635, 160);
+            this.button3.Location = new System.Drawing.Point(226, 239);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(63, 47);
+            this.button3.Size = new System.Drawing.Size(63, 18);
             this.button3.TabIndex = 6;
             this.button3.Text = "Enter\r\n";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
-            // Form1
+            // Converter
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.DarkSlateGray;
-            this.ClientSize = new System.Drawing.Size(710, 482);
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(296, 485);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
@@ -127,9 +127,8 @@
             this.Controls.Add(this.button1);
             this.Controls.Add(this.BASE64TextBox);
             this.Controls.Add(this.UTF8TextBox);
-            this.MaximumSize = new System.Drawing.Size(726, 521);
-            this.MinimumSize = new System.Drawing.Size(726, 521);
-            this.Name = "Form1";
+            this.MaximumSize = new System.Drawing.Size(1000, 1000);
+            this.Name = "Converter";
             this.ShowInTaskbar = false;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
